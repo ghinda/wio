b9461a98be14d5be55d722b27527dc06c8ba640a
Ionut Colceriu <contact@ghinda.net>
2015-03-04 00:10:19 +0200

    Lots of improvements to the dropbox adapter. Find a way around having to use a separate page/url for the oauth callback.

--- app/wio/wio-adapter-dropbox.js
+++ app/wio/wio-adapter-dropbox.js
@@ -27,6 +27,101 @@ wio.adapter('dropbox', (function() {
     return normalizedMeta;
     
   };
+  
+  var parseQueryParams = function(qstr) {
+    var query = {};
+    var a = qstr.split('&');
+    for (var i in a)
+    {
+      var b = a[i].split('=');
+      query[decodeURIComponent(b[0])] = decodeURIComponent(b[1]);
+    }
+
+    return query;
+  };
+  
+  /* check if the popup-auth window has redirected
+   * to the original url, and get the auth token from the hash.
+   */
+  var checkPopupHash = function(window) {
+    
+    var token = null;
+    
+    /* we have to use a try catch because it throws
+     * an error while in the oauth flow,
+     * and the popup has a different origin.
+     * the try-catch is ugly but avoids having to use
+     * an extra url just for the oauth callback.
+     */
+    try {
+      
+      var hash = window.location.hash.substr(1);
+      
+      // if we can (popup is on the same origin)
+      // get the token from the url
+      if(hash) {
+        token = parseQueryParams(hash);
+        
+        // close the popup
+        window.close();
+      }
+      
+    } catch(e) {}
+    
+    return token;
+    
+  };
+  
+  var popupAuthDriver = {
+    authType: function() {
+      return 'token';
+    },
+    url: function() {
+      return window.location.href;
+    },
+    doAuthorize: function(authUrl, stateParm, client, callback) {
+      
+      var popupWidth = 600;
+      var popupHeight = 500;
+      var popupLeft = (window.outerWidth - popupWidth) / 2;
+      var popupTop = (window.outerHeight - popupHeight) / 2;
+      
+      // make it look like a popup
+      // and center it onscreen
+      var authWindow = window.open(authUrl, 'wio-dropbox-auth-popup', 'location=no, width=' + popupWidth+ ', height=' + popupHeight + ', top=' + popupTop +', left=' + popupLeft);
+      
+      var authCheckInterval = setInterval(function() {
+        
+        var token = checkPopupHash(authWindow);
+        
+        if(token) {
+          clearInterval(authCheckInterval);
+          clearTimeout(authTimeout);
+          
+          callback(token);
+        }
+        
+      }, 1000);
+      
+      // if the auth flow is not done in 10s
+      // we'll consider the popup closed,
+      // and stop trying to get the hash.
+      var authTimeout = setTimeout(function() {
+          
+        clearInterval(authCheckInterval);
+        
+        callback({
+          error: 'access_denied',
+          error_description: 'OAuth authorization timeout. The authorization popup was probably closed.'
+        });
+        
+        client.reset();
+      
+      // TODO make the timeout configurable?
+      }, 10000);
+      
+    }
+  };
 
   var authorize = function(params, callback) {
 
@@ -40,7 +135,7 @@ wio.adapter('dropbox', (function() {
         
       } else {
         
-        client.authDriver(new Dropbox.AuthDriver.Popup());
+        client.authDriver(popupAuthDriver);
         
         // TODO close the popup and continue the flow after accepting
         
